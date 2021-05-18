22647a19dd5e2e61dc382d7d82d71d72468b2021
keemeesuu <kimhogy@gmail.com>
2021-05-18 11:34:16 +0900

    Initial Commit

---
+++ StudySwift/StudySwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudySwiftApp.swift
+//  StudySwift
+//
+//  Created by hogy on 2021/05/18.
+//
+
+import SwiftUI
+
+@main
+struct StudySwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
